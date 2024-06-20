609c86ffb3626166f8945caf4d376554b6d4907f
RAJESH PADHIHARY <rajeshpadhihary100@gmail.com>
2024-06-20 18:15:09 +0530

    20-06-24-RP

--- project/settings.py
+++ project/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 
 
-DATABASES = {
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "cehs",
-        'USER': "postgres",
-        'PASSWORD': "root",
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "cehs",
+#         'USER': "postgres",
+#         'PASSWORD': "root",
+#         'HOST': 'localhost',
+#         'PORT': '',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
