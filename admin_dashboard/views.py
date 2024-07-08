d5e12051f871fcd2df3e548a95d9ee667527856a
RAJESH PADHIHARY <rajeshpadhihary100@gmail.com>
2024-07-08 22:42:28 +0530

    silly-changes

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -23,5 +23,13 @@ class AdminDashboard(View):
         activity_request_obj = common_models.UserActivity.objects.filter(is_accepted='pending').order_by('-date_time')
         gardener_sell_request_obj = common_models.SellProduce.objects.filter(is_approved='pending').order_by('-date_time')
 
+        total_rt_gardeners = common_models.User.objects.filter(is_rtg = True).count()
+        total_vendors = common_models.User.objects.filter(is_vendor = True).count()
+        total_service_provider = common_models.User.objects.filter(is_serviceprovider = True).count()
+        total_activities = common_models.UserActivity.objects.all().count()
+        rtg_garden_profile = common_models.GardeningProfile.objects.all()
+        rtg_total_garden_area = sum([i.garden_area for i in rtg_garden_profile])
+        rtg_total_plants = sum([i.number_of_plants for i in rtg_garden_profile])
+
         return render(request, self.template,locals())
 
